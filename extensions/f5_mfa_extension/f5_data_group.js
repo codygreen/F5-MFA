ad43b53a5f8b1c1363b2ab92ffb2c50d5523eed6
Cody Green <cody@codygreen.com>
2016-07-22 16:41:39 -0500

    switched from node-rest-client to icontrol
    switched NPM library used for API calls to F5 icontrol

--- extensions/f5_mfa_extension/f5_data_group.js
+++ extensions/f5_mfa_extension/f5_data_group.js
@@ -1,19 +1,21 @@
-var Client = require('node-rest-client').Client;
+var iControl = require('icontrol');
+
+// set API connection and authentication 
+var bigip = new iControl({
+  host: '127.0.0.1',
+  proto: 'https',
+  port: '443',
+  username: 'admin',
+  pass: 'admin',
+  strict: 'true',
+  debug: 'true'
+});
 
 var exports = module.exports = {};
-
-// set BIG-IP specific variables
-var getURI = "https://127.0.0.1/mgmt/tm/ltm/data-group/internal/~Common~token_keys"; //on box
-//var getURI = "https://10.128.1.128/mgmt/tm/ltm/data-group/internal/~Common~google_auth_keys";
 
 //ignore self signed certificate
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-// set API authentication 
-var options_auth = {
-	user: "admin", 
-	password: "admin"
-};
 
 /**
  * handle API GET requets
@@ -66,10 +68,10 @@ exports.getData = function(key, callback) {
  * @param {Function} callback
  */
 exports.getDataGroup = function(callback) {
-  exports.get(getURI, "", function (res) {
-      callback(res);
-      //return res;
-    });
+  bigip.list('/ltm/data-group/internal/~Common~token_keys', function(err, res) {
+    callback(res);
+  });
+
 };
 
 /**
@@ -120,18 +122,8 @@ exports.put = function (key, data, callback) {
  * @param {Function} callback
  */ 
 exports.putRequest = function(url, args, callback) {
-	var client = new Client(options_auth);
-  var req = client.put(url, args, function(data, response) {
+  bigip.modify('/ltm/data-group/internal/~Common~token_keys', args, function(err, res) {
     callback(data);
-  });
-  req.on('requestTimeout', function (req) {
-  	console.log('request has expired');
-  });
-  req.on('responseTimeout', function (res) {
-  	console.log('response has expired');
-  });
-  req.on('error', function(err) {
-  	console.log('request error', err);
   });
 };
 
